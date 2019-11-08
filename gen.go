8c5b438d58640cfe8e89155fed32dea2b907ffd1
Hayden Faulds <fauldsh@gmail.com>
2019-11-08 14:48:04 +0000

    document and skip interfaces where no methods have context.Context as an argument

--- gen.go
+++ gen.go
@@ -5,25 +5,57 @@ import (
 	"strings"
 )
 
+/*
+TODO:
+imports
+method arguments
+*/
+
 func generate(interfaces []Interface) string {
 	var b strings.Builder
 	for _, iface := range interfaces {
+		// Skip interfaces where no methods have context.Context as an argument
+		if shouldSkipInterface(iface) {
+			continue
+		}
+
+		/*
+			type traceExample struct {
+				wrapped Example
+			}
+		*/
 		structName := fmt.Sprintf("trace%s", iface.Name)
 		fmt.Fprintf(&b, "type %s struct {\n", structName)
 		fmt.Fprintf(&b, "	wrapped %s\n", iface.Name)
 		fmt.Fprintf(&b, "}\n\n")
 
+		/* func NewExampleTracer(p0 Example) Example {
+			return traceExample {
+				wrapped p0,
+			}
+		}*/
 		generateMethodSig(&b, "", fmt.Sprintf("New%sTracer", iface.Name), []string{iface.Name}, []string{iface.Name})
-		fmt.Fprintf(&b, "	return %s {\n", structName)
-		fmt.Fprintf(&b, "		wrapped: %s,\n", iface.Name)
-		fmt.Fprintf(&b, "	}\n")
+		fmt.Fprintf(&b, "\treturn %s {\n", structName)
+		fmt.Fprintf(&b, "\t\twrapped: p0,\n")
+		fmt.Fprintf(&b, "\t}\n")
 		fmt.Fprintf(&b, "}\n\n")
 
-		for _, method := range iface.Methods {
-			generateMethodSig(&b, structName, method.Name, []string{}, []string{})
-			fmt.Fprint(&b, "	ctx, span := trace.ChildSpan(ctx)\n")
-			fmt.Fprint(&b, "	defer span.Close()\n")
-			fmt.Fprintf(&b, "	return	wrapped.%s()\n", method.Name)
+		/*
+			func (t traceExample) Foo(p0 context.Context, p1) error {
+				ctx, span := trace.ChildSpan(p0)
+				defer span.Close()
+				return t.wrapped(p0,p1)
+			}
+		*/
+		for i, m := range iface.Methods {
+			generateMethodSig(&b, structName, m.Name, []string{}, []string{})
+			// only add tracing if there a context
+			offset, ok := getFirstContextParamOffset(m)
+			if ok && i == offset {
+				fmt.Fprintf(&b, "\tctx, span := trace.ChildSpan(p%d)\n", offset)
+				fmt.Fprint(&b, "\tdefer span.Close()\n")
+			}
+			fmt.Fprintf(&b, "\treturn t.wrapped.%s()\n", m.Name)
 			fmt.Fprintf(&b, "}\n\n")
 		}
 	}
@@ -56,4 +88,22 @@ func generateMethodSig(b *strings.Builder, implementor, methodName string, param
 		fmt.Fprint(b, ")")
 	}
 	fmt.Fprint(b, " {\n")
+}
+
+func shouldSkipInterface(i Interface) bool {
+	for _, m := range i.Methods {
+		if _, ok := getFirstContextParamOffset(m); ok {
+			return false
+		}
+	}
+	return true
+}
+
+func getFirstContextParamOffset(m method) (int, bool) {
+	for i, p := range m.Params {
+		if p.Type == "context.Context" {
+			return i, true
+		}
+	}
+	return 0, false
 }

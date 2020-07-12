951605a390980869871d4080d7f1fc2de11dcb2d
Hayden Faulds <fauldsh@gmail.com>
2020-07-12 15:24:09 +0100

    fix tracing interface

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -4,12 +4,7 @@ import (
 	"context"
 )
 
-type StartSpanOption func(interface{})
-
-func OpName(opName string) func(interface{}) {
-	return func(_ interface{}) {}
-}
-
 type Tracing interface {
-	ChildSpan(context.Context, StartSpanOption)
+	ChildSpan(context.Context, interface{}...)
+	OpName(opName string) func(interface{})
 }
